4eefe460c502b28425d9bb5e6ff9f7ae1daa7c3a
Othman Shahrouri <othman@sitech.me>
2023-11-03 19:02:47 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Othman Shahrouri on 03/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
